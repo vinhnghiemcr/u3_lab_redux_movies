c8d6a4cd2e1e59d22d3a142db8fb7ac4d915e683
Andre Pato <anpato1994@gmail.com>
2020-12-08 16:06:29 -0500

    remove unused dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './styles/App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <h3>Redux Thunk Practice</h3>
+    </div>
+  )
 }
 
 export default App
